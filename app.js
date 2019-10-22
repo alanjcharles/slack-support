b08cc03eef58986c30217482d0cb22a7e8b47f08
Alan Charles <alan.charles@Alans-MacBook-Pro-2.local>
2019-10-21 22:27:53 -0600

    hello test

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.message('hello', ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    say({
+      blocks: [
+      {
+          "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+       }
+      ]
+    });
+  });
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
